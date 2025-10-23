0cc89ea37db37d54b59438252420cfb697862f0d
SyedAnasAzim <168578323+SyedAnasAzim@users.noreply.github.com>
2025-10-23 23:01:09 +0500

    Add files via upload

--- Client.py
+++ Client.py
@@ -1,21 +1,81 @@
 import socket
 import os
+import hashlib
+# import time
+
+def file_hash(file):
+    h = hashlib.sha256()
+    with open(file,"rb") as f:
+        while chunk:=f.read(4096):
+            h.update(chunk)
+    return h.hexdigest()
+
+def bar(current,total):
+    percent = current * 100 / total
+    b = int(percent)
+    print(f"\rProgress: {percent:.2f}% [{'=' * b}{' ' * (100 - b)}]", end="")
+
+
+def recv_until(sock,delimiter):
+    buf = bytearray()
+    data = sock.recv(1)
+    buf.extend(data)
+    while data:
+        data = sock.recv(1)
+        if data == delimiter:
+            break
+        buf.extend(data)
+    return bytes(buf)
+
+def recv_full(sock,filesize):
+    chunk_size = 4096
+    got_data = 0
+    buf = bytearray()
+    # print(filesize)
+    while filesize > got_data:
+        # time.sleep(0.01)
+        data = sock.recv(min(chunk_size,filesize-got_data))
+        if not data:
+            break
+        buf.extend(data)
+        got_data += min(chunk_size,filesize-got_data)
+        bar(got_data,filesize)
+        # print(got_data)
+    print()
+    return bytes(buf)
 
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect(("localhost",9000))
 
-images = ["ghost.jpg","gta.jpg","R9.jpg"]
-for img in images: 
-    client.sendall(f"Hi {img} {os.path.getsize(img)}\n".encode())
+# images = ["ghost.jpg","gta.jpg","R9.jpg"]
+while True:
+    command = input("$:").strip()
+    if command == "":
+        continue
+    comm_list = command.split()
+    if comm_list[0].lower() == "send":
+        file_h = file_hash(comm_list[1])
+        filesize = os.path.getsize(comm_list[1])
+        client.sendall(f"{command} {filesize} {file_h}\n".encode())
+    
+        print("msg sent")
+        try:
+            with open(comm_list[1],"rb") as f:
+                while True:
+                    data = f.read(4096)
+                    if not data:
+                        break
+                    # time.sleep(0.01)
+                    client.sendall(data)
+        except FileNotFoundError:
+            print("File doesn't exist!!!")
 
-
-    print("msg sent")
-    with open(img,"rb") as f:
-        while True:
-            data = f.read(4096)
-            if not data:
-                break
-            client.sendall(data)
+    elif comm_list[0].lower() == "exit":
+        client.sendall(command.encode())
+        break
+    else:
+        print(8)
+        client.sendall(f"{command}\n".encode())
         
 
 client.close()
--- Server.py
+++ Server.py
@@ -1,4 +1,19 @@
 import socket
+import hashlib
+# import time
+
+def file_hash(file):
+    h = hashlib.sha256()
+    with open(file,"rb") as f:
+        while chunk:=f.read(4096):
+            h.update(chunk)
+    return h.hexdigest()
+
+def bar(current,total):
+    percent = current * 100 / total
+    b = int(percent)
+    print(f"\rProgress: {percent:.2f}% [{'=' * b}{' ' * (100 - b)}]", end="")
+
 
 def recv_until(sock,delimiter):
     buf = bytearray()
@@ -15,18 +30,18 @@ def recv_full(sock,filesize):
     chunk_size = 4096
     got_data = 0
     buf = bytearray()
-    print(filesize)
+    # print(filesize)
     while filesize > got_data:
+        # time.sleep(0.01)
         data = sock.recv(min(chunk_size,filesize-got_data))
         if not data:
             break
         buf.extend(data)
         got_data += min(chunk_size,filesize-got_data)
+        bar(got_data,filesize)
         # print(got_data)
+    print()
     return bytes(buf)
-
-
-
 
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind(("localhost",9000))
@@ -36,14 +51,24 @@ print("Waiting for connection !!!")
 conn, add = server.accept()
 print(f"Connedted to [{add}]")
 
-for i in range(3):
+while True:
     msg = recv_until(conn,b"\n").decode()
     # msg = conn.recv(1024).decode()
     print(msg)
     msg = msg.split()
-    data = recv_full(conn,int(msg[2]))
-    with open(f"received_{msg[1]}.jpg","wb") as f:
-        f.write(data)
+
+    if msg[0].lower() == "send":
+        data = recv_full(conn,int(msg[2]))
+        with open(f"received_{msg[1]}","wb") as f:
+            f.write(data)
+        if msg[3] == file_hash(f"received_{msg[1]}"):
+            print("File received successfully!!!")
+        else:
+            print("File is corrupted durng transfer!!!")
+    elif msg[0].lower() == "exit":
+        break
+    else:
+        print("Wrong Command")
 
 conn.close()
 server.close()
